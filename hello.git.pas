937969388a27854f82482f4a8316a78b021a6b76
1a1van <145674131+1a1van@users.noreply.github.com>
2023-10-08 19:26:38 +0300

    Add files via upload

---
+++ hello.git.pas
@@ -0,0 +1,18 @@
+﻿program Hello;
+var
+  language: string;
+begin
+  writeln('Enter the language you speak:');
+  readln(language);
+
+  if language = 'English' then
+    writeln('Hello!')
+  else if language = 'French' then
+    writeln('Bonjour!')
+  else if language = 'Spanish' then
+    writeln('¡Hola!')
+  else
+    writeln('Sorry, I don''t know that language.');
+
+  readln;
+end.
